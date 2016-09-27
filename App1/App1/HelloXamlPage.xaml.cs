f1f600ef442737564b64668ee0de1943e45e27fa
manuqiao <manuqiao@me.com>
2016-09-27 22:14:43 +0800

    delete old project

---
+++ App1/App1/HelloXamlPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class HelloXamlPage : ContentPage
+	{
+		public HelloXamlPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
