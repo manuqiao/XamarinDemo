1002c0b5a4e743200ce5bfbd181f1e51211d7ac8
manuqiao <manuqiao@me.com>
2016-09-28 22:22:04 +0800

    simple ui elements and a list with data binding

--- HiPolePM/HiPolePM/HiPolePMPage.xaml.cs
+++ HiPolePM/HiPolePM/HiPolePMPage.xaml.cs
@@ -1,12 +1,17 @@
 ﻿using Xamarin.Forms;
-
+using System;
 namespace HiPolePM
 {
 	public partial class HiPolePMPage : ContentPage
 	{
+		int count = 0;
 		public HiPolePMPage()
 		{
 			InitializeComponent();
+		}
+		void onButtonClicked(object sender, EventArgs args)
+		{
+			//simpleLabel.Text = count++.ToString();	
 		}
 	}
 }
