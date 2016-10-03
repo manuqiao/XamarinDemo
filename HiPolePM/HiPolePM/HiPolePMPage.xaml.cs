dc8c2387b2cde96235d95a098ec55d7e85488ad1
manuqiao <manuqiao@me.com>
2016-10-03 10:26:24 +0800

    simple web request

--- HiPolePM/HiPolePM/HiPolePMPage.xaml.cs
+++ HiPolePM/HiPolePM/HiPolePMPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Xamarin.Forms;
 using System;
+using System.Net.Http;
+
 namespace HiPolePM
 {
 	public partial class HiPolePMPage : ContentPage
@@ -9,9 +11,30 @@ namespace HiPolePM
 		{
 			InitializeComponent();
 		}
-		void onButtonClicked(object sender, EventArgs args)
+		async void onButtonClicked(object sender, EventArgs args)
 		{
-			//simpleLabel.Text = count++.ToString();	
+			////simpleLabel.Text = count++.ToString();	
+			HttpClient client;
+			client = new HttpClient();
+			client.MaxResponseContentBufferSize = 256000;
+
+			var uri = new Uri(entry.Text);
+			//var uri = new Uri("http://www.baidu.com");
+
+			try
+			{
+				var response = await client.GetAsync(uri);
+				if (response.IsSuccessStatusCode)
+				{
+					var content = response.Content.ReadAsStringAsync();
+					simpleLabel.Text = response.ToString();
+					//Items = JsonConvert.DeserializeObject<List<TodoItem>>(content);
+				}
+			}
+			catch (Exception e)
+			{
+				simpleLabel.Text = e.ToString();
+			}
 		}
 	}
 }
---
+++ HiPolePM/HiPolePM/RequestModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace HiPolePM
+{
+	public class RequestModel
+	{
+		public string location { get; set; }
+		public string output
+		{
+			get
+			{
+				return "json";
+			}
+		}
+		public string ak
+		{
+			get
+			{
+				return "QrQhpn7ds2DHGIkx2bf4YPetBubwjBsN";
+			}
+		}
+
+		public RequestModel()
+		{
+			
+		}
+	}
+}
