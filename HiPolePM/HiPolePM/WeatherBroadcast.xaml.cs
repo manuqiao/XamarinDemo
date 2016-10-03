abeff0cd988982e5d11b776ecadf2de2465a0170
manuqiao <manuqiao@me.com>
2016-10-03 21:20:56 +0800

    navigation push and refresh list data

--- HiPolePM/HiPolePM/WeatherBroadcast.xaml.cs
+++ HiPolePM/HiPolePM/WeatherBroadcast.xaml.cs
@@ -7,9 +7,19 @@ namespace HiPolePM
 {
 	public partial class WeatherBroadcast : ContentPage
 	{
+		//private iCommand refreshCommand;
 		public WeatherBroadcast()
 		{
 			InitializeComponent();
+			NamedColor namedColor = new NamedColor();
+			listView.ItemsSource = namedColor.All;
+			listView.RefreshCommand = new Command (OnRefresh);
+		}
+		public void OnRefresh()
+		{
+			NamedColor namedColor = new NamedColor();
+			listView.ItemsSource = namedColor.All;
+			listView.EndRefresh ();
 		}
 	}
 }
